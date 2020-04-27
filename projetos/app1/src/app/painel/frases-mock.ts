cf32990ad3713b41be73dbb1b642fa9369f4be69
Sidão <sidaoswat@hotmail.com>
2020-04-27 00:59:07 -0300

    adding frase.model and frases of mock

---
+++ projetos/app1/src/app/painel/frases-mock.ts
@@ -0,0 +1,9 @@
+import { Frase } from "../shared/frase.model";
+
+const FRASES: Array<Frase> = [
+    { fraseEng: 'I like to learn', frasePtBr: 'Eu gosto de aprender' },
+    { fraseEng: 'I whatch tv', frasePtBr: 'Eu assisto tv' },
+    { fraseEng: 'How are you?', frasePtBr: 'Como vai você?' },
+    { fraseEng: 'I eat bread', frasePtBr: 'Eu como pão' },
+    { fraseEng: 'I love programming', frasePtBr: 'Eu amo programar' }
+];
---
+++ projetos/app1/src/app/shared/frase.model.ts
@@ -0,0 +1,14 @@
+export class Frase {
+
+    /*
+    public fraseEng: string;
+    public frasePtBr: string;
+
+    constructor(fraseEng: string, frasePtBr: string) {
+        this.fraseEng = fraseEng;
+        this.frasePtBr = frasePtBr;
+    }*///pode-se fazer assim para deixar o código mais verboso ou então dessa outra forma deixando ele mais enxuto e menos verboso
+
+    constructor(public fraseEng: string, public frasePtBr: string) { }
+    
+}
