f9a10d9432d9621a34f6f50908ce90bba8860fef
Sidão <sidaoswat@hotmail.com>
2020-05-06 16:53:17 -0300

    create componentes diversao and restaurantes

---
+++ projetos/app2/src/app/restaurantes/restaurantes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'xyz-restaurantes',
+  templateUrl: './restaurantes.component.html',
+  styleUrls: ['./restaurantes.component.css']
+})
+export class RestaurantesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
