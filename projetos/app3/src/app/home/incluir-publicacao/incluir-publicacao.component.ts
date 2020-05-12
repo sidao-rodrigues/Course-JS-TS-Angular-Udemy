610565ddf5bb37dd1acb68ff98a2587f42c18bdb
Sidão <sidaoswat@hotmail.com>
2020-05-12 20:58:58 -0300

    adding input and formGroup e formControl

--- projetos/app3/src/app/home/incluir-publicacao/incluir-publicacao.component.ts
+++ projetos/app3/src/app/home/incluir-publicacao/incluir-publicacao.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormGroup, FormControl } from "@angular/forms";
+
 @Component({
   selector: 'app-incluir-publicacao',
   templateUrl: './incluir-publicacao.component.html',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IncluirPublicacaoComponent implements OnInit {
 
+  public formulario: FormGroup = new FormGroup({
+    'titulo': new FormControl(null),
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public publicar(): void {
+    console.log('aqui');
   }
 
 }
