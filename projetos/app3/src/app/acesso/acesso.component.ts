c2a9042d2815573aef1c4e48fab10bc43631bbc9
Sidão <sidaoswat@hotmail.com>
2020-05-12 12:21:15 -0300

    implementing animations vertical

--- projetos/app3/src/app/acesso/acesso.component.ts
+++ projetos/app3/src/app/acesso/acesso.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { state, style, trigger, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-acesso',
   templateUrl: './acesso.component.html',
-  styleUrls: ['./acesso.component.css']
+  styleUrls: ['./acesso.component.css'],
+  animations: [
+    trigger('animacao-banner', [
+      state('criado', style({
+        'opacity': 1
+      })),
+      transition('void => criado', [
+        style({ 'opacity': 0, 'transform': 'translate(-100px, 0)'}),
+        animate('1s 500ms ease-in-out')
+      ])
+    ])
+  ]
 })
 export class AcessoComponent implements OnInit {
+
+  public estadoBanner: string = 'criado';
 
   constructor() { }
 
