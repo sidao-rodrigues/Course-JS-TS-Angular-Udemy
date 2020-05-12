8eb67cc8af3751c17a80d011d5f63019b23a0edd
Sidão <sidaoswat@hotmail.com>
2020-05-12 16:28:31 -0300

    create authService

--- projetos/app3/src/app/app.module.ts
+++ projetos/app3/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ReactiveFormsModule } from "@angular/forms";
 
+import { Auth } from "./auth.service";
+
 import { AppComponent } from './app.component';
 import { AcessoComponent } from './acesso/acesso.component';
 import { BannerComponent } from './acesso/banner/banner.component';
@@ -22,7 +24,7 @@ import { CadastroComponent } from './acesso/cadastro/cadastro.component';
     BrowserAnimationsModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [Auth],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ projetos/app3/src/app/auth.service.ts
@@ -0,0 +1,7 @@
+import { Usuario } from "./acesso/usuario.model";
+
+export class Auth {
+    public cadastraUsuario(usuario: Usuario) {
+        console.log('Usuario : ', usuario)
+    }
+}
