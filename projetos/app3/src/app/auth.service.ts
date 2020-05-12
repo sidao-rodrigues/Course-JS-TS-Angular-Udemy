236a29aa07dea0dc2489f36c017f01de079cefda
Sidão <sidaoswat@hotmail.com>
2020-05-12 16:46:35 -0300

    implementing login

--- projetos/app3/src/app/auth.service.ts
+++ projetos/app3/src/app/auth.service.ts
@@ -1,7 +1,16 @@
 import { Usuario } from "./acesso/usuario.model";
+import * as firebase from 'firebase';
 
 export class Auth {
     public cadastraUsuario(usuario: Usuario) {
-        console.log('Usuario : ', usuario)
+        //console.log('Usuario : ', usuario);
+
+        firebase.auth().createUserWithEmailAndPassword(usuario.email, usuario.senha)
+            .then((resposta: any) => {
+                console.log(resposta);
+            })
+            .catch((error: Error) => {
+                console.log(error);
+            });
     }
 }
