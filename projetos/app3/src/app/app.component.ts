80d66651881f50ad697a3cfe484f299689687c2a
Sidão <sidaoswat@hotmail.com>
2020-05-12 16:40:17 -0300

    configure firebase SDK in appComponent

--- projetos/app3/src/app/app.component.ts
+++ projetos/app3/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app3';
+
+  ngOnInit(): void {
+    
+    var firebaseConfig = {
+      apiKey: "AIzaSyDmrAmQmpponp6b2EEB3o37_0RysQfSkWk",
+      authDomain: "jta-instagram-clone-ea384.firebaseapp.com",
+      databaseURL: "https://jta-instagram-clone-ea384.firebaseio.com",
+      projectId: "jta-instagram-clone-ea384",
+      storageBucket: "jta-instagram-clone-ea384.appspot.com",
+      messagingSenderId: "568681938245",
+      appId: "1:568681938245:web:1d5f0ba0060ef5da32522c",
+      measurementId: "G-K33W12HH3P"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  }
 }
