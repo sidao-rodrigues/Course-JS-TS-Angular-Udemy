e0c5378fa52924bf21bec8b442a6c2794c2f66bb
Sidão <sidaoswat@hotmail.com>
2020-05-12 12:10:41 -0300

    create logic transitions of images

--- projetos/app3/src/app/acesso/banner/banner.component.ts
+++ projetos/app3/src/app/acesso/banner/banner.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { state, style, trigger, transition, animate } from "@angular/animations";
 
+import { Imagem } from "./imagem.model";
+
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
@@ -10,7 +12,7 @@ import { state, style, trigger, transition, animate } from "@angular/animations"
       state('escondido', style({
         'opacity': 0
       })),
-      state('visible', style({
+      state('visivel', style({
         'opacity': 1
       })),
       transition('escondido <=> visivel', animate('2s ease-in')), //ou =>
@@ -22,12 +24,37 @@ export class BannerComponent implements OnInit {
 
   public estado: string = 'escondido';
 
+  public imagens: Imagem[] = [
+    { estado:'visivel', url: '/assets/banner-acesso/img_1.png' },
+    { estado:'escondido', url: '/assets/banner-acesso/img_2.png' },
+    { estado:'escondido', url: '/assets/banner-acesso/img_3.png' },
+    { estado:'escondido', url: '/assets/banner-acesso/img_5.png' },
+    { estado:'escondido', url: '/assets/banner-acesso/img_4.png' }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => this.logicaRotacao(), 3000)
   }
 
-  public toggleEstado(): void {
-    this.estado = this.estado === 'visivel' ? 'escondido' : 'visivel';
+  public logicaRotacao(): void {
+
+    let aux: number
+
+    //ocultar
+    for(let x: number = 0; x <= 4; x++) {
+      if(this.imagens[x].estado === 'visivel') {
+        this.imagens[x].estado = 'escondido';
+
+        aux = x === 4 ? 0 : x + 1; //lógica para rotação de imagens
+        break;
+      }
+    }
+
+    //exibir outra
+    this.imagens[aux].estado = 'visivel';
+    setTimeout(() => this.logicaRotacao(), 3000)
   }
+
 }
