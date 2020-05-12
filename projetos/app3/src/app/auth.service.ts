729665d1f1c90c1806913485a1f34ff41ce50d90
Sidão <sidaoswat@hotmail.com>
2020-05-12 17:05:27 -0300

    implementing insert login in database firebase

--- projetos/app3/src/app/auth.service.ts
+++ projetos/app3/src/app/auth.service.ts
@@ -7,7 +7,13 @@ export class Auth {
 
         firebase.auth().createUserWithEmailAndPassword(usuario.email, usuario.senha)
             .then((resposta: any) => {
-                console.log(resposta);
+
+                //remover a senha do atributo senha do objeto usuário
+                delete usuario.senha
+
+                //registrando dados complementares do usuário no path email na base64
+                firebase.database().ref(`usuario_detalhe/${btoa(usuario.email)}`)//btoa converte para base 64 e atob volta para alfanuméricos
+                    .set(usuario);
             })
             .catch((error: Error) => {
                 console.log(error);
